8ceac05353824e12402ce6e644a556227a76c85d
Samuel Blake <samuelblake97@gmail.com>
2018-11-15 21:54:34 -0800

    Added API to retrieve user profile
    Added to the Proto file to set up another endpoint which can be used to retrieve the details pertaining to a User. Also set up the skeleton for setting up the http part of the code, but the autogenerated pb.gw file hasn't been generated yet.

--- server/server.go
+++ server/server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"net"
+	"net/http"
+	"os"
 
 	pb "github.com/UCSDIOWA/user-profile-api/protos"
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -21,12 +26,33 @@ type mongo struct {
 var DB *mongo
 
 func main() {
+	errors := make(chan error)
+
+	go func() {
+		errors <- startGRPC()
+	}()
+
+	go func() {
+		flag.Parse()
+		defer glog.Flush()
+
+		errors <- startHTTP()
+	}()
+
+	for err := range errors {
+		log.Fatal(err)
+		return
+	}
+}
+
+func startGRPC() error {
 	// Host mongo server
-	m, err := mgo.Dial("127.0.0.1:27017")
+	m, err := mgo.Dial("mongodb://tea:cse110IOWA@ds159263.mlab.com:59263/tea")
 	if err != nil {
 		log.Fatalf("Could not connect to the MongoDB server: %v", err)
 	}
 	defer m.Close()
+	log.Println("Connected to the MongoDB Server.")
 
 	DB = &mongo{m.DB("tea").C("users")} //change collection to Users
 
@@ -43,6 +69,29 @@ func main() {
 	if err := s.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+
+	return err
+}
+
+func startHTTP() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterUserProfileAPIHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+	log.Println("Listening on port 8080")
+
+	herokuPort := os.Getenv("PORT")
+	if herokuPort == "" {
+		herokuPort = "8080"
+	}
+
+	return http.ListenAndServe(":"+herokuPort, mux)
 }
 
 func (s *server) CreatePassword(ctx context.Context, crPsswdReq *pb.CreatePasswordRequest) (*pb.CreatePasswordResponse, error) {
@@ -53,4 +102,14 @@ func (s *server) CreatePassword(ctx context.Context, crPsswdReq *pb.CreatePasswo
 		return &pb.CreatePasswordResponse{Success: false}, err
 	}
 	return &pb.CreatePasswordResponse{Success: true}, nil
+}
+
+func (s *server) UserProfile(ctx context.Context, usrProfReq *pb.UserProfileRequest) (*pb.UserProfileResponse, error) {
+	userProfile := &pb.UserProfileResponse{}
+	err := DB.Operation.Find(bson.M{"email": (*usrProfReq).Email}).One(userProfile)
+	if err != nil {
+		return &pb.UserProfileResponse{Success: false}, err
+	}
+
+	return userProfile, nil
 }
